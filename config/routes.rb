58eef7fb8469a35fbddfd2552c65ba9b6c1bd75d
Edwin Caldon <edycop@gmail.com>
2022-08-10 12:52:16 -0500

    add remove action to user_stock controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   devise_for :users
 
-  resources :user_stocks, only: [:create]
+  resources :user_stocks, only: [:create, :destroy]
 
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
